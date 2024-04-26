7067de3a386fffb509d332f53a218cedc39412fb
ekremsalli <ekremsallieast@gmail.com>
2024-04-26 11:23:37 +0300

    Create drones and rente_drone added on client side

--- ui/src/pages/Drones.js
+++ ui/src/pages/Drones.js
@@ -4,6 +4,22 @@ import DataTable from 'react-data-table-component';
 
 export default function Drones({ jwt }) {
   const [products, setProducts] = useState([]);
+
+  const [formData, setFormData] = useState({
+    brand:'',
+    model:'',
+    weight: 0,
+    category: '',
+    max_altitude: 0,
+    power_source: '',
+    speed: 0,
+    departure: '',
+    landing:'',
+    length:0,
+    image:'',
+    price:'',
+    status:'',
+  });
   
   useEffect(() => {
     const fetchData = async () => {
@@ -16,12 +32,48 @@ export default function Drones({ jwt }) {
         setProducts(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
-        // Handle errors appropriately, e.g., display an error message to the user
       }
     };
 
     fetchData();
-  }, [products]); // Dependency array includes jwt for re-fetching on JWT change
+  }, [formData]); 
+
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); 
+
+    try {
+      const token = localStorage.getItem('accessToken');
+      console.log(formData)
+      const response = await axios.post('http://127.0.0.1:8000/drone/', formData, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log('Drone created successfully:', response.data);
+      setFormData({
+        brand:'',
+        model:'',
+        weight: 0,
+        category: '',
+        max_altitude: 0,
+        power_source: '',
+        speed: 0,
+        departure: '',
+        landing:'',
+        length:0,
+        image:'',
+        price:'',
+        status:'',
+      }); 
+    } catch (error) {
+      console.error('Error creating rental:', error);
+    }
+  };
   const columns = [
 	{
 		name: 'id',
@@ -69,15 +121,15 @@ export default function Drones({ jwt }) {
 	},
 	{
 		name: 'image',
-		selector: row => row.speed,
+		selector: row => row.image,
 	},
   {
 		name: 'price',
-		selector: row => row.speed,
+		selector: row => row.price,
 	},
   {
 		name: 'status',
-		selector: row => row.speed,
+		selector: row => row.status,
 	},
 ];
   return (
@@ -86,6 +138,26 @@ export default function Drones({ jwt }) {
 			columns={columns}
 			data={products}
 		/>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" id="brand" name="brand" value={formData.brand} onChange={handleInputChange} required />
+          <input type="text" id="model" name="model" value={formData.model} onChange={handleInputChange} required />
+          <input type="number" id="weight" name="weight" value={formData.weight} onChange={handleInputChange} required />
+          <input type="text" id="category" name="category" value={formData.category} onChange={handleInputChange} required />
+          <input type="number" id="max_altitude" name="max_altitude" value={formData.max_altitude} onChange={handleInputChange} required />
+          <input type="text" id="power_source" name="power_source" value={formData.power_source} onChange={handleInputChange} required />
+          <input type="number" id="speed" name="speed" value={formData.speed} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <input type="text" id="departure" name="departure" value={formData.departure} onChange={handleInputChange} required />
+          <input type="text" id="landing" name="landing" value={formData.landing} onChange={handleInputChange} required />
+          <input type="number" id="length" name="length" value={formData.length} onChange={handleInputChange} required />
+          <input type="text" id="image" name="image" value={formData.image} onChange={handleInputChange} required />
+          <input type="text" id="price" name="price" value={formData.price} onChange={handleInputChange} required />
+          <input type="text" id="status" name="status" value={formData.status} onChange={handleInputChange} required />
+        </div>
+        <button type="submit">Add Drone</button>
+      </form>
+    </div> 
   );
 }
--- ui/src/pages/Rented.js
+++ ui/src/pages/Rented.js
@@ -4,6 +4,12 @@ import DataTable from 'react-data-table-component';
 
 export default function Rented({ jwt }) {
   const [products, setProducts] = useState([]);
+  const [formData, setFormData] = useState({
+    start_date: '',
+    end_date: '',
+    drone_id: '',
+    user_id: '',
+  });
   
   useEffect(() => {
     const fetchData = async () => {
@@ -20,7 +26,29 @@ export default function Rented({ jwt }) {
     };
 
     fetchData();
-  }, [products]); // Dependency array includes jwt for re-fetching on JWT change
+  }, [formData]);
+  
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); 
+
+    try {
+      const token = localStorage.getItem('accessToken');
+      const response = await axios.post('http://127.0.0.1:8000/rented/', formData, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log('Rental created successfully:', response.data);
+      setFormData({ start_date: '', end_date: '',  }); 
+    } catch (error) {
+      console.error('Error creating rental:', error);
+    }
+  };
   const columns = [
     {
       name: 'id',
@@ -45,10 +73,19 @@ export default function Rented({ jwt }) {
 ];
   return (
     <div className="card">
+      
       <DataTable
 			columns={columns}
 			data={products}
 		/>
+     <form onSubmit={handleSubmit}>
+        <input type="date" id="start_date" name="start_date" value={formData.start_date} onChange={handleInputChange} required />
+        <input type="date" id="end_date" name="end_date" value={formData.end_date} onChange={handleInputChange} required />
+        <input type="text" id="drone_id" name="drone_id" value={formData.drone_id} onChange={handleInputChange} required />
+        <input type="text" id="user_id" name="user_id" value={formData.user_id} onChange={handleInputChange} required />
+        <button type="submit">Rent Drone</button>
+      </form>
+    
     </div>
   );
 }
